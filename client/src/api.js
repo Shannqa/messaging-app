d35a7613c4f9a755f5350236e065ddf105596eef
shannqa <116514532+Shannqa@users.noreply.github.com>
2024-05-18 20:29:52 +0200

    Prepping for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
 const API_URL_DEV = "";
-const API_URL = "https://circular-gamy-stone.glitch.me/server/";
+const API_URL = "https://circular-gamy-stone.glitch.me/server";
 
 export { API_URL, API_URL_DEV };
